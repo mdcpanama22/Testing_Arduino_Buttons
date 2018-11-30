ccf11179710a0bb258af051e2736c84f26701a89
Carlson <carlsm4@win.rpi.edu>
2018-11-30 18:24:26 -0500

    More functions added: ARDUINO TWEAKS, USB FUNCTION, and NumPad

---
+++ EGD_GDC_Game/Assets/COMPORTS.cs
@@ -0,0 +1,73 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Management; 
+internal class ProcessConnection { 
+   public static ConnectionOptions ProcessConnectionOptions()
+   {
+     ConnectionOptions options = new ConnectionOptions();
+     options.Impersonation = ImpersonationLevel.Impersonate;
+     options.Authentication = AuthenticationLevel.Default;
+     options.EnablePrivileges = true;
+     return options;
+   }
+ 
+   public static ManagementScope ConnectionScope(string machineName, ConnectionOptions options, string path)
+   {
+     ManagementScope connectScope = new ManagementScope();
+     connectScope.Path = new ManagementPath(@"\\" + machineName + path);
+     connectScope.Options = options;
+     connectScope.Connect();
+     return connectScope;
+   }
+}
+public class COMPORTS : MonoBehaviour {
+
+	 public string Name { get; set; }
+   public string Description { get; set; }
+ 
+	void Start(){
+		foreach (COMPORTS comPort in COMPORTS.GetCOMPortsInfo())
+		{
+  			Console.WriteLine(string.Format("{0} – {1}", comPort.Name, comPort.Description));
+		}
+	}
+   public COMPORTS() { }      
+ 
+   public static List<COMPORTS> GetCOMPortsInfo()
+   {
+     List<COMPORTS> COMPORTSList = new List<COMPORTS>();
+ 
+     ConnectionOptions options = ProcessConnection.ProcessConnectionOptions();
+     ManagementScope connectionScope = ProcessConnection.ConnectionScope(Environment.MachineName, options, @"\root\CIMV2");
+ 
+     ObjectQuery objectQuery = new ObjectQuery("SELECT * FROM Win32_PnPEntity WHERE ConfigManagerErrorCode = 0");
+     ManagementObjectSearcher comPortSearcher = new ManagementObjectSearcher(connectionScope, objectQuery);
+ 
+     using (comPortSearcher)
+     {
+       string caption = null;
+       foreach (ManagementObject obj in comPortSearcher.Get())
+       {
+         if (obj != null)
+         {
+           object captionObj = obj["Caption"];
+           if (captionObj != null)
+           {
+              caption = captionObj.ToString();
+              if (caption.Contains("(COM"))
+              {
+                COMPORTS COMPORTS = new COMPORTS();
+                COMPORTS.Name = caption.Substring(caption.LastIndexOf("(COM")).Replace("(", string.Empty).Replace(")",
+                                                     string.Empty);
+                COMPORTS.Description = caption;
+                COMPORTSList.Add(COMPORTS);
+              }
+           }
+         }
+       }
+     } 
+     return COMPORTSList;
+   } 
+}
+*/
--- EGD_GDC_Game/Assets/Scripts/MyMessageListener.cs
+++ EGD_GDC_Game/Assets/Scripts/MyMessageListener.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MyMessageListener : MonoBehaviour {
 
@@ -9,6 +10,8 @@ public class MyMessageListener : MonoBehaviour {
 
     public GameObject L;
     public GameObject R;
+	public GameObject T;
+	public Text CodeT;
 	// Use this for initialization
 	void Start () {
         Connected = false;	
@@ -23,8 +26,8 @@ public class MyMessageListener : MonoBehaviour {
 
     void OnMessageArrived(string m)
     {
-        //TestCommands(m);
-        Debug.Log("Arrived: " + m);
+        TestCommands(m);
+        //Debug.Log("Arrived: " + m);
 
     }
     void OnConnectionEvent(bool s)
@@ -38,11 +41,26 @@ public class MyMessageListener : MonoBehaviour {
         return Connected;
     }
 
+    bool ContainsCommand(string c, char Command){
+        foreach(char x in c){
+            if(x == Command){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
     void TestCommands(string CList)
     {
-        string[] SS = CList.Split(new string[] { "," }, StringSplitOptions.None);
-        Debug.Log(SS);
-        if (SS[0] == "H")
+        string[] SS = CList.Split(new string[] { ","}, StringSplitOptions.None);
+        String commands = "";
+        foreach(String x in SS){
+            commands += x;
+        }
+        Debug.Log(commands);
+         
+        if (commands.Contains("L"))
         {
             L.GetComponent<Renderer>().material.color = Color.yellow;
         }
@@ -50,7 +68,7 @@ public class MyMessageListener : MonoBehaviour {
         {
             L.GetComponent<Renderer>().material.color = Color.white;
         }
-        if(SS[1] == "H")
+        if(commands.Contains("R"))
         {
             R.GetComponent<Renderer>().material.color = Color.yellow;
         }
@@ -58,5 +76,50 @@ public class MyMessageListener : MonoBehaviour {
         {
             R.GetComponent<Renderer>().material.color = Color.white;
         }
+		
+		//NUMPAD STUFF you have to use Keycode.Keypad# example Keycode.Keypad7 
+		//or you can use Input.GetKeyDown("[7]") specifically for the keypad
+		
+
+		//I HAVE NO IDEA HOW TO MAKE THIS BETTER SO BARE WITH ME
+		if(Input.GetKeyDown(KeyCode.Keypad0) || Input.GetKeyDown(KeyCode.Insert)){
+			CodeT.text += "0";
+		}
+		if(Input.GetKeyDown(KeyCode.Keypad1) || Input.GetKeyDown(KeyCode.End)){
+			CodeT.text += "1";
+		}
+		if(Input.GetKeyDown(KeyCode.Keypad2) || Input.GetKeyDown(KeyCode.DownArrow)){
+			CodeT.text += "2";
+		}
+		if(Input.GetKeyDown(KeyCode.Keypad3) || Input.GetKeyDown(KeyCode.PageDown)){
+			CodeT.text += "3";
+		}
+		if(Input.GetKeyDown(KeyCode.Keypad4) || Input.GetKeyDown(KeyCode.LeftArrow)){
+			CodeT.text += "4";
+		}
+		//Keypad 5 is weird, doesn't have an alternative unless it's blank
+		if(Input.GetKeyDown(KeyCode.Keypad5)){
+			CodeT.text += "5";
+		}
+		if(Input.GetKeyDown(KeyCode.Keypad6) || Input.GetKeyDown(KeyCode.RightArrow)){
+			CodeT.text += "6";
+		}
+		if(Input.GetKeyDown(KeyCode.Keypad7) || Input.GetKeyDown(KeyCode.Home)){
+			CodeT.text += "7";
+		}
+		if(Input.GetKeyDown(KeyCode.Keypad8) || Input.GetKeyDown(KeyCode.UpArrow)){
+			CodeT.text += "8";
+		}
+		if(Input.GetKeyDown(KeyCode.Keypad9) || Input.GetKeyDown(KeyCode.PageUp)){
+			CodeT.text += "9";
+		}
+		if(CodeT.text.Length == 7){
+			if(CodeT.text == "1234567"){
+				T.GetComponent<Renderer>().material.color = Color.yellow;
+			}else if(CodeT.text == "7654321"){
+				T.GetComponent<Renderer>().material.color = Color.white;
+			}
+			CodeT.text = "";
+		}
     }
 }
