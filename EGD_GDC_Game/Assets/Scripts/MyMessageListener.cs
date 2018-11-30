778bd501242c17919c53f4a25e9ef3a0a8199e5e
Matthew Carlson <mdcpanama22@gmail.com>
2018-11-30 13:25:17 -0500

    FIRST COMMIT

---
+++ EGD_GDC_Game/Assets/Scripts/MyMessageListener.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyMessageListener : MonoBehaviour {
+
+    private bool Connected;
+
+    public GameObject L;
+    public GameObject R;
+	// Use this for initialization
+	void Start () {
+        Connected = false;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //called when Arduion sends a line of data
+
+    void OnMessageArrived(string m)
+    {
+        //TestCommands(m);
+        Debug.Log("Arrived: " + m);
+
+    }
+    void OnConnectionEvent(bool s)
+    {
+        Connected = s;
+        Debug.Log(s ? "Device Connected" : "Device Disconnected");
+    }
+
+    public bool GetConnection()
+    {
+        return Connected;
+    }
+
+    void TestCommands(string CList)
+    {
+        string[] SS = CList.Split(new string[] { "," }, StringSplitOptions.None);
+        Debug.Log(SS);
+        if (SS[0] == "H")
+        {
+            L.GetComponent<Renderer>().material.color = Color.yellow;
+        }
+        else
+        {
+            L.GetComponent<Renderer>().material.color = Color.white;
+        }
+        if(SS[1] == "H")
+        {
+            R.GetComponent<Renderer>().material.color = Color.yellow;
+        }
+        else
+        {
+            R.GetComponent<Renderer>().material.color = Color.white;
+        }
+    }
+}
